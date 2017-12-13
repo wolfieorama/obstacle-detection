2764bf08fb8a6bceb8107f358bbfd28723cb46b5
John Munyi <wachirawamunyi@gmail.com>
2017-12-13 16:55:20 +0200

    updated pyhton script

--- send_data.py
+++ send_data.py
@@ -1,21 +1,24 @@
-# import serial
+import serial
 import psycopg2
 import time
 import random
-import uuid
 
-# arduino = serial.Serial("/dev/ttyACM1")
-# arduino.baudrate =9600
+arduino = serial.Serial("/dev/ttyACM1")
+arduino.baudrate =9600
 
-conn=psycopg2.connect(database="obstacle_detection_development",user="obstacle_detection",password="obstacle_detection",host="localhost",port="5432")
+conn=psycopg2.connect(database="dakkt3c1iuntiv",user="oiedoiklmgjnit",password="0a584243f50a5c9f4fba6ef430afc1ae1681b643cf19fa833d1fbbea772dec4a",host="ec2-54-83-54-224.compute-1.amazonaws.com",port="5432")
 cur=conn.cursor()
-# while True:
-    # val=ser.readline()
-data = uuid.uuid4()
-ts = time.time()
-# st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-with conn:
-    cur.execute("""INSERT INTO detected_obstacles values(76,NOW(),87,NOW(),NOW())""")
 
-# conn.commit
+for a in arduino
+    id = random.randint(0,100000)
+    timestamps = NOW()
+    # distance = arduino.readline()
+    distance = 77
+    created_at = NOW()
+    updated_at = NOW()
+
+    query = "INSERT INTO detected_obstacles (id, timestamps, distance, created_at, updated_at) VALUES (%s, %s, %s, %s, %s);"
+    data = (id, timestamps, distance, created_at, updated_at)
+    cur.execute(query, data)
+    conn.commit
 conn.close
